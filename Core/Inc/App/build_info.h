b4dbefab01fb4a31dab7036e1091425a028d069b
MaheshArtPark7 <mahesh@artpark.in>
2022-12-16 10:41:42 +0530

    Create build_info.h

---
+++ Core/Inc/App/build_info.h
@@ -0,0 +1,15 @@
+/*
+ * build_info.h
+ *
+ *  Created on: 16-Dec-2022
+ *      Author: mahes
+ */
+
+#ifndef INC_APP_BUILD_INFO_H_
+#define INC_APP_BUILD_INFO_H_
+
+#define APP_MAJOR    0
+#define APP_MINOR    0
+#define APP_REVISION 1
+
+#endif /* INC_APP_BUILD_INFO_H_ */
