3afebc07b2dc59de02ad98cdca7228e8fe8b2af0
Diwij <diwij.narang2001@gmail.com>
2023-02-09 12:52:01 +0530

    Added tasks in threads
    Reading the FCU state every 10ms and controlling FET's based on it,
    Checking The FET NTC temp every 100ms to make sure they dont get burnt,
    Sending the Battery information from the MCU to the can line every 100ms.

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -58,14 +58,14 @@ osMessageQId can_rx_queueHandle;
 
 /* USER CODE END FunctionPrototypes */
 
-void app_task_1Hz(void const *argument);
-void app_task_10hz(void const *argument);
-void app_task_100hz(void const *argument);
+void app_task_1Hz(void const * argument);
+void app_task_10hz(void const * argument);
+void app_task_100hz(void const * argument);
 
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
 /* GetIdleTaskMemory prototype (linked to static allocation support) */
-void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize);
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize );
 
 /* USER CODE BEGIN GET_IDLE_TASK_MEMORY */
 static StaticTask_t xIdleTaskTCBBuffer;
@@ -81,57 +81,57 @@ void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer, StackTyp
 /* USER CODE END GET_IDLE_TASK_MEMORY */
 
 /**
- * @brief  FreeRTOS initialization
- * @param  None
- * @retval None
- */
-void MX_FREERTOS_Init(void)
-{
-    /* USER CODE BEGIN Init */
+  * @brief  FreeRTOS initialization
+  * @param  None
+  * @retval None
+  */
+void MX_FREERTOS_Init(void) {
+  /* USER CODE BEGIN Init */
 
-    /* USER CODE END Init */
+  /* USER CODE END Init */
 
-    /* USER CODE BEGIN RTOS_MUTEX */
+  /* USER CODE BEGIN RTOS_MUTEX */
     /* add mutexes, ... */
-    /* USER CODE END RTOS_MUTEX */
+  /* USER CODE END RTOS_MUTEX */
 
-    /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
     /* add semaphores, ... */
-    /* USER CODE END RTOS_SEMAPHORES */
+  /* USER CODE END RTOS_SEMAPHORES */
 
-    /* USER CODE BEGIN RTOS_TIMERS */
+  /* USER CODE BEGIN RTOS_TIMERS */
     /* start timers, add new ones, ... */
-    /* USER CODE END RTOS_TIMERS */
+  /* USER CODE END RTOS_TIMERS */
 
-    /* Create the queue(s) */
-    /* definition and creation of can_tx_queue */
-    osMessageQDef(can_tx_queue, 512, uint8_t);
-    can_tx_queueHandle = osMessageCreate(osMessageQ(can_tx_queue), NULL);
+  /* Create the queue(s) */
+  /* definition and creation of can_tx_queue */
+  osMessageQDef(can_tx_queue, 512, uint8_t);
+  can_tx_queueHandle = osMessageCreate(osMessageQ(can_tx_queue), NULL);
 
-    /* definition and creation of can_rx_queue */
-    osMessageQDef(can_rx_queue, 512, uint8_t);
-    can_rx_queueHandle = osMessageCreate(osMessageQ(can_rx_queue), NULL);
+  /* definition and creation of can_rx_queue */
+  osMessageQDef(can_rx_queue, 512, uint8_t);
+  can_rx_queueHandle = osMessageCreate(osMessageQ(can_rx_queue), NULL);
 
-    /* USER CODE BEGIN RTOS_QUEUES */
+  /* USER CODE BEGIN RTOS_QUEUES */
     /* add queues, ... */
-    /* USER CODE END RTOS_QUEUES */
+  /* USER CODE END RTOS_QUEUES */
 
-    /* Create the thread(s) */
-    /* definition and creation of APP_1HZ_TASK */
-    osThreadDef(APP_1HZ_TASK, app_task_1Hz, osPriorityNormal, 0, 128);
-    APP_1HZ_TASKHandle = osThreadCreate(osThread(APP_1HZ_TASK), NULL);
+  /* Create the thread(s) */
+  /* definition and creation of APP_1HZ_TASK */
+  osThreadDef(APP_1HZ_TASK, app_task_1Hz, osPriorityNormal, 0, 128);
+  APP_1HZ_TASKHandle = osThreadCreate(osThread(APP_1HZ_TASK), NULL);
 
-    /* definition and creation of APP_10HZ_TASK */
-    osThreadDef(APP_10HZ_TASK, app_task_10hz, osPriorityAboveNormal, 0, 128);
-    APP_10HZ_TASKHandle = osThreadCreate(osThread(APP_10HZ_TASK), NULL);
+  /* definition and creation of APP_10HZ_TASK */
+  osThreadDef(APP_10HZ_TASK, app_task_10hz, osPriorityAboveNormal, 0, 128);
+  APP_10HZ_TASKHandle = osThreadCreate(osThread(APP_10HZ_TASK), NULL);
 
-    /* definition and creation of APP_100HZ_TASK */
-    osThreadDef(APP_100HZ_TASK, app_task_100hz, osPriorityHigh, 0, 128);
-    APP_100HZ_TASKHandle = osThreadCreate(osThread(APP_100HZ_TASK), NULL);
+  /* definition and creation of APP_100HZ_TASK */
+  osThreadDef(APP_100HZ_TASK, app_task_100hz, osPriorityHigh, 0, 128);
+  APP_100HZ_TASKHandle = osThreadCreate(osThread(APP_100HZ_TASK), NULL);
 
-    /* USER CODE BEGIN RTOS_THREADS */
+  /* USER CODE BEGIN RTOS_THREADS */
     /* add threads, ... */
-    /* USER CODE END RTOS_THREADS */
+  /* USER CODE END RTOS_THREADS */
+
 }
 
 /* USER CODE BEGIN Header_app_task_1Hz */
@@ -141,19 +141,19 @@ void MX_FREERTOS_Init(void)
  * @retval None
  */
 /* USER CODE END Header_app_task_1Hz */
-void app_task_1Hz(void const *argument)
+void app_task_1Hz(void const * argument)
 {
-    /* USER CODE BEGIN app_task_1Hz */
+  /* USER CODE BEGIN app_task_1Hz */
     TickType_t xLastWakeTime;
     const TickType_t xFrequency = 1000;
     xLastWakeTime = xTaskGetTickCount();
     /* Infinite loop */
     for (;;)
     {
-        app_gauge_tick();
-//        vTaskDelayUntil(&xLastWakeTime, xFrequency);
+        //app_gauge_tick();
+        vTaskDelayUntil(&xLastWakeTime, xFrequency);
     }
-    /* USER CODE END app_task_1Hz */
+  /* USER CODE END app_task_1Hz */
 }
 
 /* USER CODE BEGIN Header_app_task_10hz */
@@ -163,19 +163,20 @@ void app_task_1Hz(void const *argument)
  * @retval None
  */
 /* USER CODE END Header_app_task_10hz */
-void app_task_10hz(void const *argument)
+void app_task_10hz(void const * argument)
 {
-    /* USER CODE BEGIN app_task_10hz */
+  /* USER CODE BEGIN app_task_10hz */
     TickType_t xLastWakeTime;
     const TickType_t xFrequency = 100;
     xLastWakeTime = xTaskGetTickCount();
     /* Infinite loop */
     for (;;)
     {
-
-//        vTaskDelayUntil(&xLastWakeTime, xFrequency);
+    writeCanBatVolt();  //writing battery voltage from mcu to CAN line
+    testBenchTempCheck();  //checking FET temperature using NTC
+    vTaskDelayUntil(&xLastWakeTime, xFrequency);
     }
-    /* USER CODE END app_task_10hz */
+  /* USER CODE END app_task_10hz */
 }
 
 /* USER CODE BEGIN Header_app_task_100hz */
@@ -185,9 +186,9 @@ void app_task_10hz(void const *argument)
  * @retval None
  */
 /* USER CODE END Header_app_task_100hz */
-void app_task_100hz(void const *argument)
+void app_task_100hz(void const * argument)
 {
-    /* USER CODE BEGIN app_task_100hz */
+  /* USER CODE BEGIN app_task_100hz */
     TickType_t xLastWakeTime;
     const TickType_t xFrequency = 10;
     xLastWakeTime = xTaskGetTickCount();
@@ -195,9 +196,11 @@ void app_task_100hz(void const *argument)
     for (;;)
     {
         // Battery State Machine
-//        vTaskDelayUntil(&xLastWakeTime, xFrequency);
+
+       readFCU_state();  //Getting FCU state over CAN line to control FET operations.
+       vTaskDelayUntil(&xLastWakeTime, xFrequency);
     }
-    /* USER CODE END app_task_100hz */
+  /* USER CODE END app_task_100hz */
 }
 
 /* Private application code --------------------------------------------------*/
