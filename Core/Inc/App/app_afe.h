fbed6e599fbac065aee376904d9e1602ffd234e6
Diwij <diwij.narang2001@gmail.com>
2023-02-21 12:20:17 +0530

    Revert "Merge branch 'test_CAN_AFE' into feature/CAN_Test"
    This reverts commit 1ab6eec4feb73d391e612fadb369e1f1dbcd69a4, reversing
    changes made to 568647107b1ae7dec0f082167895ebf546a5351a.

--- Core/Inc/App/app_afe.h
+++ Core/Inc/App/app_afe.h
@@ -9,21 +9,6 @@
 
 #include "app_defines.h"
 
-typedef struct
-{
-
-}TS_FETcotrol_s;
-
-typedef struct
-{
-    uint16_t Load_Voltage;        //Stores load Voltage
-    uint16_t Pack_Current;        //Stores Pack Current
-    uint8_t FET_Status;           //Stores FET Status
-    uint16_t CellVoltages;        //Stores Cell Voltages (1-10)
-    uint16_t Temperatures;        //Stores Temperatures TS1 and TS3
-
-}TS_AFEdata_s;
-
 extern int16_t app_afe_init(void);
 extern int16_t app_afe_tick(void);
 
