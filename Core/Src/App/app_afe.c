4001075ce7b591334e55c2c4fe6cf8a905adc820
saptarshi25 <saptarshi2001.c@gmail.com>
2023-02-28 14:29:04 +0530

    Update app_afe.c

--- Core/Src/App/app_afe.c
+++ Core/Src/App/app_afe.c
@@ -15,10 +15,9 @@ TS_FETcotrol_s AFE_FETcontrol;
 TS_AFEdata_s AFE_data;
 TS_AFEinfo_s AFE_info;
 
-extern uint16_t afe_data_read(void)
+int16_t afe_data_read(void)
 {
   uint16_t data=0;
-  uint8_t i,j;
   uint8_t CellVoltageHolder = Cell1Voltage;
   int16_t ret_val = SYS_ERR;
   do
@@ -35,7 +34,7 @@ extern uint16_t afe_data_read(void)
       AFE_data.temps[0] = data;
     if(SYS_OK == bq76952_dir_cmd_read(TS3Temperature, &data, 2))
       AFE_data.temps[1] = data;
-    for(i=0;i<10;i++)
+    for(uint8_t i=0;i<10;i++)
     {
       do
       {
@@ -48,14 +47,14 @@ extern uint16_t afe_data_read(void)
     }
 
     AFE_info.Device_Number = AFE_data.device_number;
-    AFE_info.Stack_Voltage = 0.01*(AFE_data.stack_vol);
-    AFE_info.Pack_Voltage = 0.01*(AFE_data.pack_vol);
-    AFE_info.Pack_Current = 0.01*(AFE_data.pack_curr);
-    AFE_info.Temperatures[0] = (float)(0.1*(AFE_data.temps[0]))-273.15;
-    AFE_info.Temperatures[1] = (float)(0.1*(AFE_data.temps[1]))-273.15;
-    for(i=0;i<10;i++)
+    AFE_info.Stack_Voltage = 0.01*(AFE_data.stack_vol);                     //Returns Stack Voltage in Volts (V)
+    AFE_info.Pack_Voltage = 0.01*(AFE_data.pack_vol);                       //Returns Pack Voltage in Volts (V)
+    AFE_info.Pack_Current = 0.01*(AFE_data.pack_curr);                      //Returns Pack Current in Amperes (A)
+    AFE_info.Temperatures[0] = (float)(0.1*(AFE_data.temps[0]))-273.15;     //Returns TS1 in  Degree Celcius (C)
+    AFE_info.Temperatures[1] = (float)(0.1*(AFE_data.temps[1]))-273.15;     //Returns TS1 in Degree Celcius (C)
+    for(uint8_t i=0;i<10;i++)
     {
-      AFE_info.CellVoltages[i] = 0.001*(AFE_data.cellvoltages[i]);
+      AFE_info.CellVoltages[i] = 0.001*(AFE_data.cellvoltages[i]);          //Returns Cell Voltages in Volts (V)
     }
     ret_val = SYS_OK;
   }while(false);
