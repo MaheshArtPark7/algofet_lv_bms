3b8ed6c561194a11354c0aee8db38a88ecc1de5a
saptarshi25 <saptarshi2001.c@gmail.com>
2023-02-28 12:54:04 +0530

    Defined structures for RAM

--- Core/Inc/App/app_afe.h
+++ Core/Inc/App/app_afe.h
@@ -16,11 +16,38 @@ typedef struct
 
 typedef struct
 {
-    uint16_t Load_Voltage;        //Stores load Voltage
-    uint16_t Pack_Current;        //Stores Pack Current
-    uint8_t FET_Status;           //Stores FET Status
-    uint16_t CellVoltages;        //Stores Cell Voltages (1-10)
-    uint16_t Temperatures;        //Stores Temperatures TS1 and TS3
+    uint16_t vCellModecmd ; //0x03C3 for 6S | 0x0303 for 4S
+    uint16_t FETs_CONTROL ;
+    uint16_t enabledProtectionsA ;
+    uint16_t enabledProtectionsB ;
+    uint16_t prechargeStartVoltage;
+    uint16_t prechargeStopVoltage;
+    uint16_t TS3config;
+
+}TS_AFEramreg_s;
+
+typedef struct
+{
+    uint16_t Device_Number;       //Stores Device Number
+    float Stack_Voltage;          //Stores Stack Voltage
+    float Pack_Voltage;           //Stores Pack Voltage
+    float Load_Voltage;           //Stores Load Voltage
+    float Pack_Current;           //Stores Pack Current
+    float CellVoltages[10];    //Stores Cell Voltages (1-10)
+    uint16_t Temperatures[2];     //Stores Temperatures TS1 and TS3
+
+}TS_AFEinfo_s;
+
+
+typedef struct
+{
+    uint16_t device_number;
+    uint16_t stack_vol;
+    uint16_t pack_vol;
+    uint16_t load_vol;  //Stores load Voltage
+    float pack_curr;        //Stores Pack Current
+    uint16_t cellvoltages[10];    //Stores Cell Voltages (1-10)
+    uint16_t temps[2];     //Stores Temperatures TS1 and TS3
 
 }TS_AFEdata_s;
 
