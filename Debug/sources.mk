c798ee9da03cc48a35c42016d72eafbee7d314d5
Diwij <diwij.narang2001@gmail.com>
2023-03-07 15:09:26 +0530

    Folder name

--- Debug/sources.mk
+++ Debug/sources.mk
@@ -22,7 +22,7 @@ C_DEPS :=
 # Every subdirectory with source files must be described here
 SUBDIRS := \
 Core/CodeGen/can/source \
-Core/Lib/BQ76952 \
+Core/Lib/BQ76942 \
 Core/Src/App \
 Core/Src \
 Core/Startup \
